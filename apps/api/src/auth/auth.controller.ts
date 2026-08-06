39aa58d27581cd7dcb7e50e78fe79eb922d6d619
Sarthak <sarthakbehera09@gmail.com>
2026-08-06 22:31:48 +0530

    feat: created initial schema structure and initial auth module

---
+++ apps/api/src/auth/auth.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  @Post('register')
+  async register() {}
+
+  @Post('login')
+  async login() {}
+}
---
+++ apps/api/src/user/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserDto } from './user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createUser(user: UserDto) {
+    const res = await this.prisma.user.create({
+      data: user,
+    });
+    return res;
+  }
+}
